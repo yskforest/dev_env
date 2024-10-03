9dbc7f237eb5fc5fe027311bf030f9a098dae92c
korver58 <ktokyoforest@yahoo.co.jp>
2024-10-03 23:47:45 +0900

    update

--- setup.sh
+++ setup.sh
@@ -2,5 +2,11 @@
  
 python -m venv dev_env
 dev_env\Scripts\activate
+
+# 仮想環境に入っているかを確認
+if [ -z "$VIRTUAL_ENV" ]; then
+    echo "Error: This script must be run within the virtual environment."
+    exit 1
+fi
 python -m pip install --upgrade pip
 pip install -r requirements.txt
